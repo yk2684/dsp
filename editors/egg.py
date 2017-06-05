590a5339b082d060f75643d5c0136993158806b7
Yukino Kondo <yukino.kondo@gmail.com>
2017-06-05 15:28:00 -0400

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("I love eggs the most.")
+print("This file was created using the emacs editor.")
+print("I think I will continue using vim, instead of emcas.")
---
+++ editors/vinegar.py
@@ -0,0 +1,2 @@
+print("This file was created using the  vim editor.")
+print("I also love vinegar")
